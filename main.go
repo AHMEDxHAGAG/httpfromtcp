e40d8a9ca7722b9ea49fcf983844226eea013db9
AHMEDxHAGAG <ahmedheagag23@gmail.com>
2026-09-07 04:51:59 +0300

    Listen to TCP Connection and Parse Lines

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"net"
 )
 
 func main() {
-	file, err := os.Open("messages.txt")
+	listener, err := net.Listen("tcp", "127.0.0.1:42069")
 	if err != nil {
+		fmt.Printf("Error: %s\n", err)
 		return
 	}
-	ch := getLinesChannel(file)
-	for str := range ch {
-		fmt.Printf("read: %s\n", str)
+	defer func() {
+		_ = listener.Close()
+	}()
+	for {
+		con, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Error: %s\n", err)
+			return
+		}
+		fmt.Println("Connection Started")
+		ch := getLinesChannel(con)
+		for str := range ch {
+			fmt.Println(str)
+		}
+		fmt.Println("Connection Closed")
 	}
 }
