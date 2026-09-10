799d87a896c6db9e42f8db7a4255457df3f1a2a7
AHMEDxHAGAG <ahmedheagag23@gmail.com>
2026-09-10 10:49:28 +0300

    Finish the Request Parser

--- internal/request/request.go
+++ internal/request/request.go
@@ -2,8 +2,10 @@
 package request
 
 import (
+	"errors"
 	"fmt"
 	"io"
+	"slices"
 )
 
 type status int
@@ -13,27 +15,47 @@ const (
 	done
 )
 
+const bufferSize = 8
+
 type Request struct {
 	Status      status
 	RequestLine RequestLine
 }
 
 func RequestFromReader(reader io.Reader) (*Request, error) {
-	buffer, err := io.ReadAll(reader)
-	if err != nil {
-		return nil, err
+	r := &Request{Status: intialized}
+	buffer := make([]byte, bufferSize)
+	readInd, sizeT := 0, bufferSize
+	for {
+		n, err := reader.Read(buffer[readInd:cap(buffer)])
+		readInd += n
+		if err != nil && !errors.Is(err, io.EOF) {
+			return nil, err
+		}
+		if errors.Is(err, io.EOF) {
+			r.Status = done
+			break
+		}
+		_, err = r.parse(buffer)
+		if err != nil {
+			return nil, err
+		}
+		if r.Status == done {
+			break
+		}
+		if n == 0 {
+			buffer = slices.Grow(buffer, sizeT)
+			sizeT *= 2
+			buffer = buffer[:cap(buffer)]
+		}
+
 	}
-	reqLine, err := parseRequestLine(buffer)
-	if err != nil {
-		return nil, err
-	}
-	return &Request{
-		RequestLine: reqLine,
-	}, nil
+	return r, nil
 }
 
 func (r *Request) parse(data []byte) (int, error) {
-	if r.Status == intialized {
+	switch r.Status {
+	case intialized:
 		rq, no, err := parseRequestLine(data)
 		if err != nil {
 			return 0, err
@@ -44,9 +66,9 @@ func (r *Request) parse(data []byte) (int, error) {
 			return no, nil
 		}
 		return 0, nil
-	} else if r.Status == done {
+	case done:
 		return 0, fmt.Errorf("error: trying to read data in a done state")
-	} else {
+	default:
 		return 0, fmt.Errorf("error: unknown state")
 	}
 }
